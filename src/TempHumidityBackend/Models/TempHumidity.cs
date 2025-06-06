de06e98451daa26485922b6ca0ac7c8fe9348ef0
Cory Coward <ccoward80@gmail.com>
2025-06-05 23:48:41 -0400

    Restructure project, add xunit test project

---
+++ src/TempHumidityBackend/Models/TempHumidity.cs
@@ -0,0 +1,22 @@
+﻿namespace TempHumidityBackend.Models;
+
+/// <summary>
+/// A class that represents data read from a temperature/humidity sensor.
+/// </summary>
+public partial class TempHumidity
+{
+    /// <summary>
+    /// The temperature reading, in Celsius.
+    /// </summary>
+    public float TempCelsius { get; set; }
+
+    /// <summary>
+    /// The relative humidity reading.
+    /// </summary>
+    public float Humidity { get; set; }
+
+    /// <summary>
+    /// The timestamp of this reading.
+    /// </summary>
+    public DateTime ReadAt { get; set; }
+}
